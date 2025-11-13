a4f9bc04f6110255de04c8620e82cb1ab9199ea1
dnhfan <dinhan.0321@gmail.com>
2025-11-13 19:07:35 +0700

    Update: using blink cmp

--- .config/nvim/lua/dnhfan/core/init.lua
+++ .config/nvim/lua/dnhfan/core/init.lua
@@ -1,2 +1,159 @@
 require("dnhfan.core.options")
 require("dnhfan.core.keymaps")
+
+local M = {}
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.b.win_separator = true
+vim.b.border_color = "#ffffff"
+vim.b.transparent_background = false
+vim.b.thick_win_separator = false
+vim.b.float_window_brightness = 0
+-- "bold": Bold line box.
+-- "double": Double-line box.
+-- "none": No border.
+-- "rounded": Like "single", but with rounded corners ("╭" etc.).
+-- "shadow": Drop shadow effect, by blending with the background.
+-- "single": Single-line box.
+-- "solid": Adds padding by a single whitespace cell.
+vim.b.doc_border_type = "" ---@type "" | "single" | "double" | "rounded" | "solid" | "shadow" | "none"
+vim.opt.background = "dark"
+vim.b.better_cmp_cursor_line = false
+vim.b.colorscheme = ""
+vim.b.syn_sidebar_with_normal = false
+vim.b.line_space = 0
+vim.o.foldlevel = 99 --Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+M.setup = function(params)
+  if params.win_separator ~= nil then
+    vim.b.win_separator = params.win_separator
+  end
+
+  if params.border_color ~= nil then
+    vim.b.border_color = params.border_color
+  end
+
+  if params.transparent_background ~= nil then
+    vim.b.transparent_background = params.transparent_background
+  end
+
+  if params.mapleader ~= nil then
+    vim.g.mapleader = params.mapleader
+  end
+
+  if params.float_window_brightness ~= nil then
+    vim.b.float_window_brightness = params.float_window_brightness
+  end
+
+  if params.doc_border_type ~= nil then
+    vim.b.doc_border_type = params.doc_border_type
+  end
+
+  if params.better_cmp_cursor_line ~= nil then
+    vim.b.better_cmp_cursor_line = params.better_cmp_cursor_line
+  end
+
+  if params.background_theme ~= nil then
+    vim.opt.background = params.background_theme
+  end
+
+  if params.colorscheme ~= nil then
+    vim.b.colorscheme = params.colorscheme
+  end
+
+  if params.thick_win_separator ~= nil then
+    vim.b.thick_win_separator = params.thick_win_separator
+  end
+
+  if params.syn_sidebar_with_normal ~= nil then
+    vim.b.syn_sidebar_with_normal = params.syn_sidebar_with_normal
+  end
+
+  if params.line_space ~= nil then
+    vim.b.line_space = params.line_space
+  end
+end
+
+M.getHexColor = function(highlight)
+  local color = vim.api.nvim_get_hl_by_name(highlight, true)
+  if not color then
+    return { background = "NONE", foreground = "NONE" }
+  end
+  return {
+    background = color.background and ("#" .. string.format("%06x", color.background)) or "NONE",
+    foreground = color.foreground and ("#" .. string.format("%06x", color.foreground)) or "NONE",
+  }
+end
+
+M.addBrightnessToHexColor = function(hexColor, brightness)
+  if brightness == nil then
+    brightness = 0
+  end
+
+  -- Remove the hash symbol if present
+  local hex = hexColor:gsub("#", "")
+
+  -- Extract red, green, blue components from the hex string
+  local r = tonumber(hex:sub(1, 2), 16)
+  local g = tonumber(hex:sub(3, 4), 16)
+  local b = tonumber(hex:sub(5, 6), 16)
+
+  -- Calculate the adjustment factor
+  local factor = (100 + brightness) / 100
+
+  -- Adjust the brightness
+  r = math.min(255, math.floor(r * factor))
+  g = math.min(255, math.floor(g * factor))
+  b = math.min(255, math.floor(b * factor))
+
+  -- Reassemble into a hex string and return
+  return string.format("#%02X%02X%02X", r, g, b)
+end
+
+M.complementaryColor = function(hexColor)
+  -- Kiểm tra xem mã màu có dấu # không, nếu không có, thêm dấu #
+  if string.sub(hexColor, 1, 1) ~= "#" then
+    hexColor = "#" .. hexColor
+  end
+
+  -- Loại bỏ dấu # từ mã màu
+  hexColor = string.sub(hexColor, 2)
+
+  -- Chuyển đổi mã màu từ hex sang RGB
+  local r = tonumber(string.sub(hexColor, 1, 2), 16)
+  local g = tonumber(string.sub(hexColor, 3, 4), 16)
+  local b = tonumber(string.sub(hexColor, 5, 6), 16)
+
+  -- Tính toán màu bổ sung (complementary)
+  local comp_r = 255 - r
+  local comp_g = 255 - g
+  local comp_b = 255 - b
+
+  -- Chuyển đổi màu bổ sung (complementary) từ RGB sang hex
+  return string.format("#%02X%02X%02X", comp_r, comp_g, comp_b)
+end
+
+M.round = function(num, numDecimalPlaces)
+  local mult = 10 ^ (numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
+M.trunc = function(hide_width, no_ellipsis)
+  return function(str)
+    -- local win_width = vim.fn.winwidth(0)
+    local win_width = vim.o.columns
+    local trunc_width = M.round(win_width * 1 / 3.2)
+    local string_length = #str + 2
+    if hide_width and win_width < hide_width then
+      return ""
+    elseif trunc_width and trunc_width < string_length then
+      return str:sub(1, #str / 2) .. (no_ellipsis and "" or "...")
+    end
+    return str
+  end
+end
+
+return M
---
+++ .config/nvim/lua/dnhfan/plugins/ui/blink-cmp.lua
@@ -0,0 +1,246 @@
+return {
+  {
+    "saghen/blink.compat",
+    version = "*",
+    lazy = true,
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        event = "InsertEnter",
+        opts = { updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
+          require("luasnip.loaders.from_vscode").lazy_load({ exclude = vim.g.vscode_snippets_exclude or {} })
+          require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.g.vscode_snippets_path or "" })
+          -- snipmate format
+          require("luasnip.loaders.from_snipmate").load()
+          require("luasnip.loaders.from_snipmate").lazy_load({ paths = vim.g.snipmate_snippets_path or "" })
+          -- lua format
+          require("luasnip.loaders.from_lua").load()
+          require("luasnip.loaders.from_lua").lazy_load({ paths = vim.g.lua_snippets_path or "" })
+          vim.api.nvim_create_autocmd("InsertLeave", {
+            callback = function()
+              if
+                require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                and not require("luasnip").session.jump_active
+              then
+                require("luasnip").unlink_current()
+              end
+            end,
+          })
+        end,
+      },
+    },
+    version = "*",
+    opts = {
+      enabled = function()
+        local filetypes = {
+          "toggleterm",
+          "NvimTree",
+          "Telescope",
+          "TelescopePrompt",
+          "Avante",
+          "AvanteInput",
+          "AvantePromptInput",
+        }
+
+        return not vim.tbl_contains(filetypes, vim.bo.filetype)
+      end,
+      keymap = {
+        preset = "none",
+        ["<CR>"] = { "select_and_accept", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<C-d>"] = { "scroll_documentation_down" },
+        ["<C-u>"] = { "scroll_documentation_up" },
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        trigger = {
+          prefetch_on_insert = false,
+          show_in_snippet = true,
+          show_on_keyword = true,
+          show_on_trigger_character = true,
+          show_on_insert_on_trigger_character = true,
+          show_on_accept_on_trigger_character = true,
+          show_on_x_blocked_trigger_characters = { "'", '"', "(" },
+        },
+        accept = {
+          create_undo_point = true,
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          cmdline_position = function()
+            if vim.g.ui_cmdline_pos ~= nil then
+              local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+              return { pos[1] - 1, pos[2] }
+            end
+            local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+            return { vim.o.lines - height, 0 }
+          end,
+          auto_show = true,
+          scrollbar = true,
+          scrolloff = 2,
+          border = require("dnhfan.core.utils").border,
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          draw = {
+            padding = 1,
+            gap = 2,
+            treesitter = {
+              "lsp",
+              "luasnip",
+              "snippets",
+              "path",
+              "buffer",
+              "cmdline",
+            },
+            columns = {
+              { "kind_icon" },
+              { "label", "label_description", gap = 1 },
+              -- { 'source_name' },
+            },
+            components = {
+              source_name = {
+                text = function(ctx)
+                  return ctx.source_name:gsub("^%l", string.upper)
+                end,
+                highlight = "BlinkCmpSource",
+              },
+              kind_icon = {
+                text = function(ctx)
+                  local kind_icon = require("dnhfan.core.utils").nv_chad_icons[ctx.kind]
+                  return kind_icon
+                end,
+              },
+            },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          treesitter_highlighting = true,
+          auto_show_delay_ms = 200,
+          window = {
+            border = require("dnhfan.core.utils").border,
+            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          },
+        },
+        ghost_text = { enabled = false },
+        list = {
+          max_items = 20,
+          selection = {
+            preselect = true,
+          },
+        },
+      },
+      signature = {
+        enabled = true,
+        window = {
+          treesitter_highlighting = true,
+          border = require("dnhfan.core.utils").border,
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+        },
+      },
+      snippets = {
+        preset = "luasnip",
+        expand = function(snippet)
+          require("luasnip").lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require("luasnip").jumpable(filter.direction)
+          end
+          return require("luasnip").in_snippet()
+        end,
+        jump = function(direction)
+          require("luasnip").jump(direction)
+        end,
+      },
+      cmdline = {
+        enabled = true,
+        keymap = {
+          preset = "none",
+          ["<CR>"] = { "select_and_accept", "fallback" },
+          ["<C-j>"] = { "select_next", "fallback" },
+          ["<C-k>"] = { "select_prev", "fallback" },
+          ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+          ["<C-e>"] = { "hide" },
+          ["<C-d>"] = { "scroll_documentation_down" },
+          ["<C-u>"] = { "scroll_documentation_up" },
+        },
+        sources = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" then
+            return { "cmdline", "path" }
+          end
+          return {}
+        end,
+        completion = {
+          trigger = {
+            show_on_blocked_trigger_characters = {},
+            show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+          },
+          list = {
+            selection = {
+              preselect = true,
+            },
+          },
+          menu = {
+            auto_show = true, -- Inherits from top level `completion.menu.auto_show` config when not set
+            draw = {
+              columns = {
+                { "kind_icon" },
+                { "label", "label_description", gap = 1 },
+                -- { 'source_name' },
+              },
+            },
+          },
+        },
+      },
+      sources = {
+        default = {
+          "lsp",
+          "snippets",
+          "path",
+          "buffer",
+        },
+        providers = {
+          lsp = { name = "LSP" },
+          snippets = { name = "snippets" },
+          path = { name = "path" },
+          cmdline = {},
+          buffer = {
+            name = "buffer",
+            max_items = 10,
+            opts = {
+              get_bufnrs = function()
+                local bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                  bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+                return vim.tbl_keys(bufs)
+              end,
+            },
+          },
+        },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
