f0bdb815b45197099e9988077256bd5f713bdf7a
dnhfan <dinhan.0321@gmail.com>
2025-11-27 08:34:00 +0700

    fix: indent err in blade file

--- .config/nvim/lua/dnhfan/plugins/treesitter.lua
+++ .config/nvim/lua/dnhfan/plugins/treesitter.lua
@@ -1,41 +1,56 @@
 -- nvim-treesitter: make a CST (concrete sytax tree)
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = require("dnhfan.core.utils").treesitter_parsers,
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
 
-      auto_install = true,
-      ignore_install = {},
-      modules = {},
-      sync_install = false,
-
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+        highlight = {
+          enable = true,
         },
-      },
-    })
+        -- enable indentation
+        indent = {
+          enable = true,
+          disable = { "blade" },
+        },
+        -- ensure these language parsers are installed
+        ensure_installed = require("dnhfan.core.utils").treesitter_parsers,
 
-    -- use bash parser for zsh files
-    vim.treesitter.language.register("bash", "zsh")
-  end,
+        auto_install = true,
+        ignore_install = {},
+        modules = {},
+        sync_install = false,
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+
+      -- use bash parser for zsh files
+      vim.treesitter.language.register("bash", "zsh")
+    end,
+
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == table then
+        vim.list_extend(opts.ensure_installed, { "blade", "php_only" })
+      end
+    end,
+  },
+
+  {
+    "jwalton512/vim-blade",
+    ft = "blade", -- Chỉ load khi mở file blade
+  },
 }
