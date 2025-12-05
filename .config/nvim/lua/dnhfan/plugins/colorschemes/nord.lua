f9d4c8902bc8897dfe33d3a524c3449ee38105e2
dnhfan <dinhan.0321@gmail.com>
2025-12-05 20:58:52 +0700

    chore: refomart

--- .config/nvim/lua/dnhfan/plugins/colorschemes/nord.lua
+++ .config/nvim/lua/dnhfan/plugins/colorschemes/nord.lua
@@ -1,14 +1,15 @@
-return vim.b.colorscheme ~= "nord" and {} or {
-  "shaunsingh/nord.nvim",
-  priority = 100,
-  config = function ()
-    vim.g.nord_contrast = false
-    vim.g.nord_borders = false
-    vim.g.nord_disable_background = false
-    vim.g.nord_italic = false
-    vim.g.nord_uniform_diff_background = false
-    vim.g.nord_bold = true
+return vim.b.colorscheme ~= "nord" and {}
+  or {
+    "shaunsingh/nord.nvim",
+    priority = 100,
+    config = function()
+      vim.g.nord_contrast = false
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = false
+      vim.g.nord_italic = false
+      vim.g.nord_uniform_diff_background = false
+      vim.g.nord_bold = true
 
-    vim.cmd.colorscheme("nord")
-  end
-}
+      vim.cmd.colorscheme("nord")
+    end,
+  }
