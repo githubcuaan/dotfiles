d694e80ed46a948d6a29a88dccb99d1dca18942e
dnhfan <dinhan.0321@gmail.com>
2025-12-05 18:26:22 +0700

    feat(nvim): not use nvim-lint for python

--- .config/nvim/lua/dnhfan/plugins/editing/linting.lua
+++ .config/nvim/lua/dnhfan/plugins/editing/linting.lua
@@ -4,65 +4,26 @@ return {
   config = function()
     local lint = require("lint")
 
+    -- 1. Setup Linter
     lint.linters_by_ft = {
-      python = { "pylint" },
+      python = {},
+      -- javascript = { "eslint_d" }, -- Ví dụ nếu sau này dùng thêm
     }
 
+    -- 2. Setup Auto Command (Tự động Lint)
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-    local function file_in_cwd(file_name)
-      return vim.fs.find(file_name, {
-        upward = true,
-        stop = vim.loop.cwd():match("(.+)/"),
-        path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
-        type = "file",
-      })[1]
-    end
-
-    local function remove_linter(linters, linter_name)
-      for k, v in pairs(linters) do
-        if v == linter_name then
-          linters[k] = nil
-          break
-        end
-      end
-    end
-
-    local function linter_in_linters(linters, linter_name)
-      for k, v in pairs(linters) do
-        if v == linter_name then
-          return true
-        end
-      end
-      return false
-    end
-
-    local function remove_linter_if_missing_config_file(linters, linter_name, config_file_name)
-      if linter_in_linters(linters, linter_name) and not file_in_cwd(config_file_name) then
-        remove_linter(linters, linter_name)
-      end
-    end
-
-    local function try_linting()
-      local linters = lint.linters_by_ft[vim.bo.filetype]
-
-      -- if linters then
-      --   -- remove_linter_if_missing_config_file(linters, "eslint_d", ".eslintrc.cjs")
-      --   remove_linter_if_missing_config_file(linters, "eslint_d", "eslint.config.js")
-      -- end
-
-      lint.try_lint(linters)
-    end
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        try_linting()
+        -- Gọi hàm này không cần tham số, nó tự check filetype và linter tương ứng
+        lint.try_lint()
       end,
     })
 
+    -- 3. Keymap thủ công (Cho chắc cốp)
     vim.keymap.set("n", "<leader>lt", function()
-      try_linting()
-    end, { desc = "Linting: Trigger linting for current file" })
+      lint.try_lint()
+    end, { desc = "Linting: Trigger linting cho file hiện tại." })
   end,
 }
