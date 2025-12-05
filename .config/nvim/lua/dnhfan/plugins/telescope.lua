0034a51cd2a42e8483544a82b3f6ee0ae22f4fac
dnhfan <dinhan.0321@gmail.com>
2025-12-05 20:59:08 +0700

    update telescope

--- .config/nvim/lua/dnhfan/plugins/telescope.lua
+++ .config/nvim/lua/dnhfan/plugins/telescope.lua
@@ -8,7 +8,6 @@ return {
   -- Plugin chính của Telescope
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- tăng tốc
