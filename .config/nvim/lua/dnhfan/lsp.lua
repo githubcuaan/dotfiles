d6f310b97858ea845a045dbb2be7c8db7e1cafbd
dnhfan <dinhan.0321@gmail.com>
2025-12-07 18:17:41 +0700

    chore(nvim): using vietnamese for desc

--- .config/nvim/lua/dnhfan/lsp.lua
+++ .config/nvim/lua/dnhfan/lsp.lua
@@ -7,47 +7,47 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local opts = { buffer = ev.buf, silent = true }
 
     -- set keybinds
-    opts.desc = "Show LSP references"
+    opts.desc = "LSP: Hiển thị tham chiếu"
     keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-    opts.desc = "Go to declaration"
+    opts.desc = "LSP: Di chuyển tới khai báo"
     keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-    opts.desc = "Show LSP definition"
+    opts.desc = "LSP: Hiển thị định nghĩa"
     keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definition
 
-    opts.desc = "Show LSP implementations"
+    opts.desc = "LSP: Hiển thị triển khai"
     keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-    opts.desc = "Show LSP type definitions"
+    opts.desc = "LSP: Hiển thị định nghĩa kiểu"
     keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-    opts.desc = "See available code actions"
+    opts.desc = "LSP: Xem hành động code khả dụng"
     keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-    opts.desc = "Smart rename"
+    opts.desc = "LSP: Đổi tên thông minh"
     keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-    opts.desc = "Show buffer diagnostics"
+    opts.desc = "LSP: Hiển thị chẩn đoán buffer"
     keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-    opts.desc = "Show line diagnostics"
+    opts.desc = "LSP: Hiển thị chẩn đoán dòng"
     keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-    opts.desc = "Go to previous diagnostic"
+    opts.desc = "LSP: Di chuyển tới chẩn đoán trước"
     keymap.set("n", "[d", function()
       vim.diagnostic.jump({ count = -1, float = true })
     end, opts) -- jump to previous diagnostic in buffer
     --
-    opts.desc = "Go to next diagnostic"
+    opts.desc = "LSP: Di chuyển tới chẩn đoán tiếp theo"
     keymap.set("n", "]d", function()
       vim.diagnostic.jump({ count = 1, float = true })
     end, opts) -- jump to next diagnostic in buffer
 
-    opts.desc = "Show documentation for what is under cursor"
+    opts.desc = "LSP: Hiển thị tài liệu tại con trỏ"
     keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-    opts.desc = "Restart LSP"
+    opts.desc = "LSP: Khởi động lại"
     keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
   end,
 })
