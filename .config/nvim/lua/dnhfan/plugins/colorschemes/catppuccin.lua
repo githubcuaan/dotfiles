59e2e435944ae528636895a9845d8652e9118007
dnhfan <dinhan.0321@gmail.com>
2025-12-14 14:07:57 +0700

    fix(nvim): catppuccin load alway

--- .config/nvim/lua/dnhfan/plugins/colorschemes/catppuccin.lua
+++ .config/nvim/lua/dnhfan/plugins/colorschemes/catppuccin.lua
@@ -1,11 +1,12 @@
 -- catppuccin theme
 
-return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
-}
+return vim.b.colorscheme ~= "catppuccin" and {}
+  or {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  }
