5b57466d9e4d53d0832f5c1898dcbec64d2da1c9
dnhfan <dinhan.0321@gmail.com>
2025-12-14 14:09:51 +0700

    feat(nvim): new shortcut

--- .config/nvim/lua/dnhfan/core/keymaps.lua
+++ .config/nvim/lua/dnhfan/core/keymaps.lua
@@ -2,6 +2,17 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
+-- movement remaps
+keymap.set({ "n", "v" }, "E", "$", {
+  desc = "Move to end of line",
+  silent = true,
+})
+
+keymap.set({ "n", "v" }, "B", "^", {
+  desc = "Move to head of line",
+  silent = true,
+})
+
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
