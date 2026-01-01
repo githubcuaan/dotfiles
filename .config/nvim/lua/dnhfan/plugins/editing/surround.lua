f7164d93fc9c2a6cd0ddb21a1e500018a4a2aad9
dnhfan <dinhan.0321@gmail.com>
2026-01-01 12:01:02 +0700

    fix(nvim): surround - sovlve conflict key in visual map ( S->Z)

--- .config/nvim/lua/dnhfan/plugins/editing/surround.lua
+++ .config/nvim/lua/dnhfan/plugins/editing/surround.lua
@@ -3,4 +3,9 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   config = true,
+  opts = {
+    keymaps = {
+      visual = "Z",
+    },
+  },
 }
