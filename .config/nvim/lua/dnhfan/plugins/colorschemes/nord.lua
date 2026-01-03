832d8e6c9cc7e602eb7aae94dfb471551fc82fbc
dnhfan <dinhan.0321@gmail.com>
2026-01-03 17:54:54 +0700

    feat(nvim): add transaction backgroud

--- .config/nvim/lua/dnhfan/plugins/colorschemes/nord.lua
+++ .config/nvim/lua/dnhfan/plugins/colorschemes/nord.lua
@@ -1,15 +1,15 @@
 return vim.b.colorscheme ~= "nord" and {}
-  or {
-    "shaunsingh/nord.nvim",
-    priority = 100,
-    config = function()
-      vim.g.nord_contrast = false
-      vim.g.nord_borders = true
-      vim.g.nord_disable_background = false
-      vim.g.nord_italic = false
-      vim.g.nord_uniform_diff_background = false
-      vim.g.nord_bold = true
+    or {
+      "shaunsingh/nord.nvim",
+      priority = 100,
+      config = function()
+        vim.g.nord_contrast = false
+        vim.g.nord_borders = true
+        vim.g.nord_disable_background = true
+        vim.g.nord_italic = false
+        vim.g.nord_uniform_diff_background = false
+        vim.g.nord_bold = true
 
-      vim.cmd.colorscheme("nord")
-    end,
-  }
+        vim.cmd.colorscheme("nord")
+      end,
+    }
