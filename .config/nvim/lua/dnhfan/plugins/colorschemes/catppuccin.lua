5743dca12ccbca3eff9141ea3c45944aa5bb5685
dnhfan <dinhan.0321@gmail.com>
2026-01-03 17:55:02 +0700

    style

--- .config/nvim/lua/dnhfan/plugins/colorschemes/catppuccin.lua
+++ .config/nvim/lua/dnhfan/plugins/colorschemes/catppuccin.lua
@@ -1,12 +1,12 @@
 -- catppuccin theme
 
 return vim.b.colorscheme ~= "catppuccin" and {}
-  or {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  }
+    or {
+      "catppuccin/nvim",
+      lazy = false,
+      name = "catppuccin",
+      priority = 1000,
+      config = function()
+        vim.cmd.colorscheme("catppuccin")
+      end,
+    }
