2b118b6978eb2e41bb9be1142b18db95f4e09e67
dnhfan <dinhan.0321@gmail.com>
2026-01-05 00:26:11 +0700

    fix(nvim): new treesiter, new conf, idk how :(

--- .config/nvim/lua/dnhfan/plugins/treesitter.lua
+++ .config/nvim/lua/dnhfan/plugins/treesitter.lua
@@ -1,71 +1,46 @@
 -- nvim-treesitter: make a CST (concrete sytax tree)
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-
-    config = function()
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.embedded_template = {
-        install_info = {
-          url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
-          files = { "src/parser.c" },
-          branch = "main",
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = "ejs",
-      }
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
-
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = {
-          enable = true,
-          disable = { "blade" },
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = require("dnhfan.core.utils").treesitter_parsers,
-
-        auto_install = true,
-        ignore_install = {},
-        modules = {},
-        sync_install = false,
-
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<leader>v",
-            node_incremental = "<leader>v",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-
-      vim.filetype.add({ extension = { ejs = "ejs" } })
-      vim.treesitter.language.register("embedded_template", "ejs")
-      vim.treesitter.language.register("html", "ejs")
-      -- vim.treesitter.language.register("javascript", "ejs")
-      -- use bash parser for zsh files
-      vim.treesitter.language.register("bash", "zsh")
-    end,
-
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == table then
-        vim.list_extend(opts.ensure_installed, { "blade", "php_only" })
-      end
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false, -- QUAN TRỌNG: Không được dùng lazy-loading
+  build = ":TSUpdate",
+  dependencies = {
+    { "jwalton512/vim-blade", ft = "blade" },
   },
+  config = function()
+    local nvim_ts = require("nvim-treesitter")
 
-  {
-    "jwalton512/vim-blade",
-    ft = "blade", -- Chỉ load khi mở file blade
-  },
+    --  1. Setup mặc định (cần thiết để plugin hoạt động)
+    nvim_ts.setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    --  2. Tự động cài đặt danh sách parser
+    nvim_ts.install(require("dnhfan.core.utils").treesitter_parsers)
+
+    -- Register bash cho zsh
+    vim.treesitter.language.register("bash", "zsh")
+
+    --  3. Bật Highlighting (Native)
+    vim.api.nvim_create_autocmd("FileType", {
+      -- Dùng pattern = "*" để nó tự nhận diện mọi file có parser cho nhàn
+      pattern = "*",
+      callback = function()
+        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+        if lang then
+          pcall(vim.treesitter.start)
+        end
+      end,
+    })
+
+    --  4. Bật Indentation (API mới)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "lua", "javascript", "php", "typescript", "json" }, -- Liệt kê list cần thiết
+      callback = function()
+        -- Không dùng cho blade vì đại ca đã disable từ trước
+        if vim.bo.filetype ~= "blade" then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+  end,
 }
