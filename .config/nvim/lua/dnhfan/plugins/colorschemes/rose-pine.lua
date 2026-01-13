d6cd07ce524e8afd0320c80b3563df8c7509046d
dnhfan <dinhan.0321@gmail.com>
2026-01-13 10:16:08 +0700

    feat(nvim): theme

--- .config/nvim/lua/dnhfan/plugins/colorschemes/rose-pine.lua
+++ .config/nvim/lua/dnhfan/plugins/colorschemes/rose-pine.lua
@@ -3,7 +3,7 @@ return vim.b.colorscheme ~= "rose-pine" and {} or {
   priority = 100,
   config = function()
     require("rose-pine").setup({
-      -- variant = "dawn", -- auto, main, moon, or dawn
+      variant = "dawn", -- auto, main, moon, or dawn
       -- dark_variant = "moon", -- main, moon, or dawn
       dim_inactive_windows = false,
       extend_background_behind_borders = false,
@@ -15,7 +15,7 @@ return vim.b.colorscheme ~= "rose-pine" and {} or {
       styles = {
         bold = true,
         italic = false,
-        transparency = false,
+        transparency = true,
       },
       groups = {
         -- border = "muted",
