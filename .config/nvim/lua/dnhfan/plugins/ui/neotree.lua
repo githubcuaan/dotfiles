252d32bb7169e2384593f518238a2c2ccbe6be01
dnhfan <dinhan.0321@gmail.com>
2026-01-19 20:28:21 +0700

    using snacks

--- .config/nvim/lua/dnhfan/plugins/ui/neotree.lua
+++ .config/nvim/lua/dnhfan/plugins/ui/neotree.lua
@@ -1,113 +1,113 @@
 -- neotree filesystem
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  lazy = false,
-
-  config = function()
-    if vim.g.colors_name == "nord" then
-      vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#D8DEE9", bg = "#2E3440" })
-      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#88C0D0", bg = "#2E3440" })
-    end
-    -- 1. Keymaps outside setup
-    vim.keymap.set("n", "<leader>e", ":Neotree toggle filesystem left<CR>", {
-      desc = "Neotree: Đóng/Mở filesystem",
-      silent = true,
-    })
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { desc = "Neotree: Xem các Buffer" })
-
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      default_position = "left",
-
-      -- 2. Mappings inside neotree window
-      window = {
-        width = 36,
-        mappings = {
-          ["l"] = "open",
-          ["h"] = "close_node",
-          ["<cr>"] = "open",
-          ["<space>"] = "toggle_node",
-          ["q"] = "close_window",
-          ["a"] = "add",
-          ["d"] = "delete",
-          ["r"] = "rename",
-          ["?"] = "show_help",
-        },
-      },
-
-      -- 3. Components
-      default_component_configs = {
-
-        indent = {
-          indent_size = 2,
-          padding = 1,
-          with_markers = true,
-          indent_marker = "│",
-          last_indent_marker = "└",
-        },
-
-        icon = {
-          default = "",
-          symlink = "",
-          bookmark = "󰆤",
-          hidden = "󰜌",
-          folder_closed = "",
-          folder_empty = "",
-          folder_empty_open = "",
-          folder_open = "",
-          use_filtered_colors = true,
-        },
-
-        modified = {
-          symbol = require("dnhfan.core.utils").sign_icons.change,
-        },
-
-        git_status = {
-          symbols = {
-            unmerged = "",
-            added = require("dnhfan.core.utils").sign_icons.add,
-            modified = require("dnhfan.core.utils").sign_icons.change,
-            deleted = require("dnhfan.core.utils").sign_icons.topdelete,
-            renamed = require("dnhfan.core.utils").sign_icons.info,
-            untracked = require("dnhfan.core.utils").sign_icons.untracked,
-            ignored = require("dnhfan.core.utils").sign_icons.warning,
-            unstaged = require("dnhfan.core.utils").sign_icons.change,
-            staged = require("dnhfan.core.utils").sign_icons.success,
-            conflict = "",
-          },
-        },
-      },
-
-      -- 4. Filesystem
-      filesystem = {
-        follow_current_file = {
-          enabled = true,
-        },
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_by_name = {
-            "node_modules",
-            ".git",
-          },
-          never_show = {
-            ".DS_Store",
-            "thumbs.db",
-          },
-        },
-      },
-
-      git_status = {
-        enable = true,
-      },
-    })
-  end,
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- branch = "v3.x",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "MunifTanjim/nui.nvim",
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
+  -- lazy = false,
+  --
+  -- config = function()
+  --   if vim.g.colors_name == "nord" then
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#D8DEE9", bg = "#2E3440" })
+  --     vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#88C0D0", bg = "#2E3440" })
+  --   end
+  --   -- 1. Keymaps outside setup
+  --   vim.keymap.set("n", "<leader>e", ":Neotree toggle filesystem left<CR>", {
+  --     desc = "Neotree: Đóng/Mở filesystem",
+  --     silent = true,
+  --   })
+  --   vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { desc = "Neotree: Xem các Buffer" })
+  --
+  --   require("neo-tree").setup({
+  --     close_if_last_window = true,
+  --     default_position = "left",
+  --
+  --     -- 2. Mappings inside neotree window
+  --     window = {
+  --       width = 36,
+  --       mappings = {
+  --         ["l"] = "open",
+  --         ["h"] = "close_node",
+  --         ["<cr>"] = "open",
+  --         ["<space>"] = "toggle_node",
+  --         ["q"] = "close_window",
+  --         ["a"] = "add",
+  --         ["d"] = "delete",
+  --         ["r"] = "rename",
+  --         ["?"] = "show_help",
+  --       },
+  --     },
+  --
+  --     -- 3. Components
+  --     default_component_configs = {
+  --
+  --       indent = {
+  --         indent_size = 2,
+  --         padding = 1,
+  --         with_markers = true,
+  --         indent_marker = "│",
+  --         last_indent_marker = "└",
+  --       },
+  --
+  --       icon = {
+  --         default = "",
+  --         symlink = "",
+  --         bookmark = "󰆤",
+  --         hidden = "󰜌",
+  --         folder_closed = "",
+  --         folder_empty = "",
+  --         folder_empty_open = "",
+  --         folder_open = "",
+  --         use_filtered_colors = true,
+  --       },
+  --
+  --       modified = {
+  --         symbol = require("dnhfan.core.utils").sign_icons.change,
+  --       },
+  --
+  --       git_status = {
+  --         symbols = {
+  --           unmerged = "",
+  --           added = require("dnhfan.core.utils").sign_icons.add,
+  --           modified = require("dnhfan.core.utils").sign_icons.change,
+  --           deleted = require("dnhfan.core.utils").sign_icons.topdelete,
+  --           renamed = require("dnhfan.core.utils").sign_icons.info,
+  --           untracked = require("dnhfan.core.utils").sign_icons.untracked,
+  --           ignored = require("dnhfan.core.utils").sign_icons.warning,
+  --           unstaged = require("dnhfan.core.utils").sign_icons.change,
+  --           staged = require("dnhfan.core.utils").sign_icons.success,
+  --           conflict = "",
+  --         },
+  --       },
+  --     },
+  --
+  --     -- 4. Filesystem
+  --     filesystem = {
+  --       follow_current_file = {
+  --         enabled = true,
+  --       },
+  --       filtered_items = {
+  --         visible = true,
+  --         hide_dotfiles = false,
+  --         hide_gitignored = true,
+  --         hide_by_name = {
+  --           "node_modules",
+  --           ".git",
+  --         },
+  --         never_show = {
+  --           ".DS_Store",
+  --           "thumbs.db",
+  --         },
+  --       },
+  --     },
+  --
+  --     git_status = {
+  --       enable = true,
+  --     },
+  --   })
+  -- end,
 }
