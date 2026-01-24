a9d30d58ebfcaa2fe2e3a00238de9b21a4d5567d
dnhfan <dinhan.0321@gmail.com>
2026-01-24 21:28:46 +0700

    feat: adjust some setting :)
    - decrease the timeout -> faster keybind
    - add localoptions in storing buffer session

--- .config/nvim/lua/dnhfan/core/options.lua
+++ .config/nvim/lua/dnhfan/core/options.lua
@@ -62,3 +62,8 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+vim.opt.timeoutlen = 300
+-- Thêm "localoptions" vào cuối danh sách để nó lưu mấy cái setting riêng của buffer
+vim.opt.sessionoptions =
+{ "blank", "buffers", "curdir", "folds", "help", "tabpages", "winsize", "winpos", "terminal", "localoptions" }
