9efe89af1ab778987bf43fb6a1d6fbc9a10ea28d
dnhfan <dinhan.0321@gmail.com>
2026-05-14 15:10:21 +0700

    feat(nvim): note taking with nvim

--- .config/nvim/lua/dnhfan/plugins/tools/plantuml.lua
+++ .config/nvim/lua/dnhfan/plugins/tools/plantuml.lua
@@ -7,6 +7,7 @@ vim.filetype.add({
     plantuml = "plantuml",
   },
 })
+vim.g["plantuml_previewer#plantuml_jar_path"] = "/usr/share/java/plantuml/plantuml.jar"
 
 return {
   "weirongxu/plantuml-previewer.vim",
@@ -15,9 +16,31 @@ return {
     "aklt/plantuml-syntax",
   },
   ft = "plantuml",
+
   config = function()
-    -- Đặt phím tắt để mở preview nhanh chóng (tùy chọn)
+    local lspconfig = require("lspconfig")
+    local configs = require("lspconfig.configs")
+
+    if not configs.plantuml_lsp then
+      configs.plantuml_lsp = {
+        default_config = {
+          cmd = {
+            "plantuml-lsp",
+            "--jar-path=/usr/share/java/plantuml/plantuml.jar",
+          },
+          filetypes = { "plantuml" },
+          root_dir = function(fname)
+            -- Cập nhật API mới không bị deprecate
+            local git_path = vim.fs.find('.git', { path = fname, upward = true })[1]
+            return git_path and vim.fs.dirname(git_path) or vim.fs.dirname(fname)
+          end,
+          settings = {},
+        }
+      }
+    end
+
     vim.keymap.set("n", "<leader>pu", ":PlantumlOpen<CR>", { desc = "Mở PlantUML Preview" })
     vim.keymap.set("n", "<leader>ps", ":PlantumlStop<CR>", { desc = "Dừng PlantUML Preview" })
-  end,
+    lspconfig.plantuml_lsp.setup {}
+  end
 }
