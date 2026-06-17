5765d46a0508a79998c4a361dc2003740145ea0d
dnhfan <dinhan.0321@gmail.com>
2026-06-17 19:00:05 +0700

    dinamic background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,13 @@
 require("dnhfan.core").setup({
   transparent_background = true,
   win_separator = true,
-  background_theme = "dark",
+  -- background_theme = "dark", -- set dynamically by dnhfan.core.dark-mode
   thick_win_separator = true, -- đổi thành false để giảm tải rendering
   syn_sidebar_with_normal = true,
   float_window_brightness = 40,
   line_space = 0,
-  -- colorscheme = "catppuccin-latte",
-  colorscheme = "catppuccin",
-  -- colorscheme = "rose-pine",
-  -- colorscheme = "nord",
-  -- colorscheme = "nightfly",
-  -- colorscheme = "nightcity",
-  -- colorscheme = "everforest",
-  -- colorscheme = "eldritch",
+  colorscheme = "rose-pine",
 })
+
+require("dnhfan.core.dark-mode").setup()
 require("dnhfan.lazy")
