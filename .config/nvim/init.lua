413285e3d63a9dd69b50bc2ce6319a674cf30e99
dnhfan <dinhan.0321@gmail.com>
2026-06-27 13:16:40 +0700

    feat(nvim): using local config for nvim setup func

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
-require("dnhfan.core").setup({
+local defaults = {
   transparent_background = true,
   win_separator = true,
   thick_win_separator = true, -- đổi thành false để giảm tải rendering
@@ -6,7 +6,16 @@ require("dnhfan.core").setup({
   float_window_brightness = 40,
   line_space = 0,
   colorscheme = "catppuccin",
-})
+}
+
+local ok, local_config = pcall(require, "dnhfan.local")
+if ok and local_config then
+  for k, v in pairs(local_config) do
+    defaults[k] = v
+  end
+end
+
+require("dnhfan.core").setup(defaults)
 
 require("dnhfan.core.dark-mode").setup()
 require("dnhfan.lazy")
