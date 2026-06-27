9a8bfcac6701c8d76859ecda27194b481ac16d71
dnhfan <dinhan.0321@gmail.com>
2026-06-27 12:08:18 +0700

    add blink compat(idk what it for)

--- .config/nvim/lua/dnhfan/plugins/editor/blink-cmp.lua
+++ .config/nvim/lua/dnhfan/plugins/editor/blink-cmp.lua
@@ -40,4 +40,21 @@ return {
     },
     opts_extend = { "sources.default" },
   },
+  {
+    -- add blink.compat
+    {
+      'saghen/blink.compat',
+      -- use v2.* for blink.cmp v1.*
+      version = '2.*',
+      -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+      lazy = true,
+      -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+      opts = {},
+    },
+
+    {
+      'saghen/blink.cmp',
+      version = '1.*',
+    },
+  }
 }
