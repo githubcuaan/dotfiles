e9beb8ae3e2c6671f55370a3250b6bf2fe171d34
dnhfan <dinhan.0321@gmail.com>
2026-06-27 12:43:12 +0700

    remove nosuse config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,11 @@
 require("dnhfan.core").setup({
   transparent_background = true,
   win_separator = true,
-  -- background_theme = "dark", -- set dynamically by dnhfan.core.dark-mode
   thick_win_separator = true, -- đổi thành false để giảm tải rendering
   syn_sidebar_with_normal = true,
   float_window_brightness = 40,
   line_space = 0,
-  colorscheme = "everforest",
+  colorscheme = "catppuccin",
 })
 
 require("dnhfan.core.dark-mode").setup()
