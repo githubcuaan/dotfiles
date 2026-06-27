7ff9abf06e0061fcb11be62bfb775c1ea97e542d
dnhfan <dinhan.0321@gmail.com>
2026-06-27 13:23:57 +0700

    styling

--- .config/nvim/lua/dnhfan/configs/opencode/keymap.lua
+++ .config/nvim/lua/dnhfan/configs/opencode/keymap.lua
@@ -1,14 +1,14 @@
 -- Recommended/example keymaps
 vim.keymap.set({ "n", "x" }, "<leader>ca", function() require("opencode").ask("@this: ") end,
-  { desc = "Ask OpenCode…" })
-vim.keymap.set({ "n", "x" }, "<leader>cs", function() require("opencode").select() end, { desc = "Select OpenCode…" })
+  { desc = " OpenCode: Ask" })
+vim.keymap.set({ "n", "x" }, "<leader>cs", function() require("opencode").select() end, { desc = " OpenCode: Select" })
 
 vim.keymap.set({ "n", "x" }, "<leader>cg", function() return require("opencode").operator("@this ") end,
-  { desc = "Add range to opencode", expr = true })
+  { desc = " OpenCode: Add range", expr = true })
 vim.keymap.set("n", "<leader>cG", function() return require("opencode").operator("@this ") .. "_" end,
-  { desc = "Add line to opencode", expr = true })
+  { desc = " OpenCode: Add line", expr = true })
 
 vim.keymap.set("n", "<A-u>", function() require("opencode").command("session.half.page.up") end,
-  { desc = "Scroll opencode up" })
+  { desc = " OpenCode: Scroll up" })
 vim.keymap.set("n", "<A-d>", function() require("opencode").command("session.half.page.down") end,
-  { desc = "Scroll opencode down" })
+  { desc = " OpenCode: Scroll down" })
