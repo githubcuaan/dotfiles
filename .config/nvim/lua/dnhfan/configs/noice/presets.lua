6d5ff6f69c4d657953fe9ef8eeabebc483f89509
dnhfan <dinhan.0321@gmail.com>
2026-06-29 00:25:17 +0700

    feat(nvim): presets conf for noice

---
+++ .config/nvim/lua/dnhfan/configs/noice/presets.lua
@@ -0,0 +1,8 @@
+return {
+  bottom_search = false,
+  command_palette = false,
+  long_message_to_split = false,
+  inc_rename = false,
+  lsp_doc_border = false,
+  cmdline_output_to_split = false,
+}
