8989bc42285f02c15a63159c2a305cbc70a291be
dnhfan <dinhan.0321@gmail.com>
2026-07-29 21:50:11 +0700

    refactor(mini-files):using a var for require mini-files module

--- .config/nvim/lua/dnhfan/plugins/configs/mini-files/keymap.lua
+++ .config/nvim/lua/dnhfan/plugins/configs/mini-files/keymap.lua
@@ -1,12 +1,13 @@
 return function()
+  local mf = require('mini.files')
+
   vim.keymap.set('n', '<leader>e', function()
-    local mf = require('mini.files')
     if not mf.close() then
       mf.open(vim.api.nvim_buf_get_name(0))
     end
   end, { desc = "Toggle mini.files" })
 
   vim.keymap.set('n', '<leader>E', function()
-    require('mini.files').open(vim.uv.cwd())
+    mf.open(vim.uv.cwd())
   end, { desc = "Open mini.files (cwd)" })
 end
