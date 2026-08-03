34962cdb01b5ccae943fd929fcc018b2708b18e1
dnhfan <dinhan.0321@gmail.com>
2026-08-04 06:44:21 +0700

    new lualine header

--- .config/nvim/lua/dnhfan/plugins/ui/luaLine.lua
+++ .config/nvim/lua/dnhfan/plugins/ui/luaLine.lua
@@ -43,7 +43,7 @@ return {
           },
           {
             function()
-              return "dnhfan"
+              return "NEOVIM"
             end,
             icon = "",
             -- color = function()
