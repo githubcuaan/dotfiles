c794f3d1cb914c00909eeeec254750a9092659f3
dnhfan <dinhan.0321@gmail.com>
2026-09-11 00:07:47 +0700

    feat(nvim): keymap group for flutter

--- .config/nvim/lua/dnhfan/plugins/configs/snacks/key.lua
+++ .config/nvim/lua/dnhfan/plugins/configs/snacks/key.lua
@@ -10,8 +10,8 @@ return {
   { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
   { "<leader>ff",      function() Snacks.picker.files() end,                                   desc = "Find Files" },
   { "<leader>fg",      function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
-  { "<leader>fp",      function() Snacks.picker.projects() end,                                desc = "Projects" },
-  { "<leader>fr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
+  -- { "<leader>fp",      function() Snacks.picker.projects() end,                                desc = "Projects" },
+  { "<leader>sr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
 
   -- git
   { "<leader>gF",      function() Snacks.picker.git_branches() end,                            desc = "Git Branches" },
