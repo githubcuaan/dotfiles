70140513b27227dd06b1b044e4bf774482c13c7d
dnhfan <dinhan.0321@gmail.com>
2026-09-21 15:49:58 +0700

    fix(nvim): lazy loading is not working with smart-splits, roll back to old version

--- .config/nvim/lua/dnhfan/plugins/navigation/smart-splits.lua
+++ .config/nvim/lua/dnhfan/plugins/navigation/smart-splits.lua
@@ -1,19 +1,22 @@
 return {
   "mrjones2014/smart-splits.nvim",
-  keys = {
-    { "<C-h>", function() require("smart-splits").move_cursor_left() end, desc = "Move left" },
-    { "<C-j>", function() require("smart-splits").move_cursor_down() end, desc = "Move down" },
-    { "<C-k>", function() require("smart-splits").move_cursor_up() end, desc = "Move up" },
-    { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Move right" },
-    { "<M-h>", function() require("smart-splits").resize_left() end, desc = "Resize left" },
-    { "<M-j>", function() require("smart-splits").resize_down() end, desc = "Resize down" },
-    { "<M-k>", function() require("smart-splits").resize_up() end, desc = "Resize up" },
-    { "<M-l>", function() require("smart-splits").resize_right() end, desc = "Resize right" },
-  },
+  lazy = false,
   config = function()
-    require("smart-splits").setup({
+    local smart_splits = require("smart-splits")
+
+    smart_splits.setup({
       default_amount = 3, -- Mỗi lần resize nhảy 3 cột/dòng
       at_edge = 'wrap',   -- Kịch lề sẽ nhảy sang phía đối diện
     })
+
+    vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left, { desc = "Move left" })
+    vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down, { desc = "Move down" })
+    vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up, { desc = "Move up" })
+    vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right, { desc = "Move right" })
+
+    vim.keymap.set('n', '<M-h>', smart_splits.resize_left, { desc = "Resize left" })
+    vim.keymap.set('n', '<M-j>', smart_splits.resize_down, { desc = "Resize down" })
+    vim.keymap.set('n', '<M-k>', smart_splits.resize_up, { desc = "Resize up" })
+    vim.keymap.set('n', '<M-l>', smart_splits.resize_right, { desc = "Resize right" })
   end,
 }
